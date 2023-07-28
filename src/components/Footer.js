678ade79213d826b9bb4c451cf36413eca22128d
RitbanBarua <ritbanbarua2@gmail.com>
2023-07-28 13:32:29 +0530

    Cleaned junks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ export default function Footer(props) {
     return (
         <>
             <footer className={`d-flex flex-wrap justify-content-between align-items-center py-3 px-5 my-4 border-top border-dark-subtle footer-${props.mode}`}>
-                <p className="col-md-4 mb-0 text-muted">© 2022 Company, Inc</p>
+                <p className="col-md-5 mb-0 mx-2 text-muted text-center">© 2023 - Made with ❤️ by <a className='col-primary' id='footer-git' target='_blank' rel='noreferrer' href="https://github.com/RitbanBarua">Ritban Barua</a></p>
 
-                <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                {/* <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
                     <svg className="bi me-2" width="40" height="32"><use xlinkHref="#bootstrap"/></svg>
-                </a>
+                </a> */}
 
                 <ul className="nav col-md-4 justify-content-end">
                     <li className="nav-item"><Link to="/" className="nav-link px-2 text-muted">Home</Link></li>
